b7e7f89c67f4975014d7b242d9190fb5c155dd5e
Omar Camay <camayomar25@gmail.com>
2025-11-21 03:48:36 +0800

    fix: remove the AuthorViewSet and TagViewSet

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, PostViewSet, CommentViewSet, TagViewSet
+from .views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
-router.register(r'authors', AuthorViewSet)
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
-router.register(r'tags', TagViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
